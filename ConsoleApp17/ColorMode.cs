f51d744f36c2d44e1d1502f60377f162e5304907
友光 草場 <tomokusaba@tomokusaba.onmicrosoft.com>
2024-01-18 06:15:31 +0900

    プロジェクト ファイルを追加します。

---
+++ ConsoleApp17/ColorMode.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.SemanticKernel;
+using System.ComponentModel;
+
+namespace ConsoleApp17;
+
+public class ColorMode
+{
+    public bool IsOn { get; set; } = false;
+
+    [KernelFunction, Description("画面モードの状態を取得します。")]
+    public string GetMode()
+    {
+        return IsOn ? "黄色" : "通常";
+    }
+
+    [KernelFunction, Description("画面モードを切り替えます。")]
+    public string ToggleMode()
+    {
+        IsOn = !IsOn;
+        if (IsOn)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;           
+        }
+        else
+        {
+            Console.ResetColor();
+        }
+
+
+        return GetMode();
+    }
+}
---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using Microsoft.SemanticKernel;
+using ConsoleApp17;
+
+IConfigurationRoot config = new ConfigurationBuilder()
+    .AddEnvironmentVariables()
+    .AddUserSecrets<Program>()
+    .Build();
+
+string deploymentName = config["OpenAI:DeploymentName"] ?? throw new InvalidOperationException("OpenAI:DeploymentName is not set.");
+string modelId = config["OpenAI:ModelId"] ?? throw new InvalidOperationException("OpenAI:ModelId is not set.");
+string endpoint = config["OpenAI:Endpoint"] ?? throw new InvalidOperationException("OpenAI:BaseUrl is not set.");
+string key = config["OpenAI:Key"] ?? throw new InvalidOperationException("OpenAI:Key is not set.");
+
+Kernel kernel = Kernel.CreateBuilder()
+ .AddAzureOpenAIChatCompletion(
+    deploymentName,
+    endpoint,
+    key).Build();
+
+kernel.Plugins.AddFromType<ColorMode>();
+
+OpenAIPromptExecutionSettings? setting = new()
+{
+    ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions,
+
+};
+
+while (true)
+{
+    Console.Write("User > ");
+    string input = Console.ReadLine()!;
+    if (input == "exit")
+    {
+        break;
+    }
+    else
+    {
+        var result = await kernel.InvokePromptAsync(input, new(setting));
+        Console.WriteLine($"Assistant > {result}");
+    }
+}
